0597f834c3f9aa4cc92fa0d5c0735593cef3f3b8
Wouter de Vos <wouter@codaisseur.com>
2016-09-19 15:32:09 +0200

    Store currentUser in localStorage

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,8 +1,10 @@
 // src/reducers/currentUser.js
 import { UPDATE_CURRENT_USER } from '../actions/update-current-user'
 
-export default function updateCurrentUser(state = null, { type, payload }) {
+export default function updateCurrentUser(
+  state = JSON.parse(localStorage.getItem('memoryUser') || 'null'), { type, payload }) {
   if (type === UPDATE_CURRENT_USER) {
+    localStorage.setItem('memoryUser', JSON.stringify(payload))
     return payload
   } else {
     return state
